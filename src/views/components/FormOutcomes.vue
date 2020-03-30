332dbece86c6bf40341b6dcbb7cba43ee3833abd
Wenxin Hu <happyhwx888@googlemail.com>
2020-03-30 13:42:19 +0200

    identify text field for dynamic forms

--- src/views/components/FormOutcomes.vue
+++ src/views/components/FormOutcomes.vue
@@ -1,167 +1,224 @@
 <template>
-<div>
-    <div style="text-align:left; font-size:26px;"><b>Inhalte und Leistungen</b></div>
-<div>
-<div class="md-layout md-gutter">
-<div class="md-layout-item md-size-33">
-      <md-field>
-        <label>Modulkürzel</label>
-        <md-input
-          v-model="modKuerzel"
-          name="modKuerzel"
-          id="modKuerzel"
-          md-dense disabled>
-        </md-input>
-      </md-field>
+  <div>
+    <div style="text-align:left; font-size:26px;">
+      <b>Inhalte und Leistungen</b>
     </div>
-</div>
-
-<div class="md-layout md-gutter">
-    <div class="md-layout-item md-size-50">
-        <md-field>
-        <label>Anzahl Lernergebnisse</label>
-        <md-input v-model="type"></md-input>
-        </md-field>
-    </div>
-
-    <div class="md-layout-item md-size-50" v-for="(input,i) in inputs1" :key="i">
-        <md-field>
-        <label>Art Lernergebnis</label>
-        <md-input v-model="input.name"></md-input>
-            <span>
-                <i class="fas fa-minus-circle" @click="remove(i)" v-show="i || ( !i && inputs1.length > 1)"></i>
-                <i class="fas fa-plus-circle" @click="add(i)" v-show="i == inputs1.length-1"></i>
-            </span>
-        </md-field>
-    </div>
-</div>
-
-<div class="md-layout md-gutter">
-    <div class="md-layout-item md-size-50">
-        <md-field>
-        <label>Anzahl Inhaltselemente</label>
-        <md-input v-model="type"></md-input>
-        </md-field>
-    </div>
-
-    <div class="md-layout-item md-size-50" v-for="(input,i) in inputs2" :key="i">
-        <md-field>
-        <label>Art Inhaltselement</label>
-        <md-input v-model="input.name"></md-input>
-            <span>
-                <i class="fas fa-minus-circle" @click="remove(i)" v-show="i || ( !i && inputs2.length > 1)"></i>
-                <i class="fas fa-plus-circle" @click="add(i)" v-show="i == inputs2.length-1"></i>
-            </span>
-        </md-field>
-    </div>
-</div>
-
-<div class="md-layout md-gutter">
-    <div class="md-layout-item md-size-50">
-        <md-field>
-        <label>Anzahl Prüfungsvorleistungen</label>
-        <md-input v-model="type"></md-input>
-        </md-field>
-    </div>
-
-    <div class="md-layout-item md-size-50" v-for="(input,i) in inputs2" :key="i">
-        <md-field>
-        <label>Art Prüfungsvorleistungen</label>
-        <md-input v-model="input.name"></md-input>
-            <span>
-                <i class="fas fa-minus-circle" @click="remove(i)" v-show="i || ( !i && inputs2.length > 1)"></i>
-                <i class="fas fa-plus-circle" @click="add(i)" v-show="i == inputs2.length-1"></i>
-            </span>
-        </md-field>
-    </div>
-</div>
-
-<div class="md-layout md-gutter">
-    <div class="md-layout-item md-size-50">
-        <md-field>
-        <label>Anzahl Prüfungsleistungen</label>
-        <md-input v-model="type"></md-input>
-        </md-field>
-    </div>
-
-    <div class="md-layout-item md-size-50" v-for="(input,i) in inputs2" :key="i">
-        <md-field>
-        <label>Art Prüfungsvorleistungen</label>
-        <md-input v-model="input.name"></md-input>
-            <span>
-                <i class="fas fa-minus-circle" @click="remove(i)" v-show="i || ( !i && inputs2.length > 1)"></i>
-                <i class="fas fa-plus-circle" @click="add(i)" v-show="i == inputs2.length-1"></i>
-            </span>
-        </md-field>
-    </div>
-</div>
-
-<div class="md-layout md-gutter">
-    <div class="md-layout-item md-size-100">
-      <md-field>
-        <label>Bloomsche Taxonomie</label>
-        <md-select
-          v-model="modType"
-          name="modulType"
-          id="modulType"
-          md-dense>
-          <md-option>erschaffen</md-option>
-          <md-option>evaluieren</md-option>
-          <md-option>analysieren</md-option>
-          <md-option>anwenden</md-option>
-          <md-option>verstehen</md-option>
-          <md-option>erinnern</md-option>
-        </md-select>
-      </md-field>
-    </div>
-</div>
-</div>
-<div class="md-layout md-gutter">
-<div class="md-layout-item">
     <div>
-    <md-button type="primary">Speichern</md-button>
-    <md-button type="secondary">Download</md-button>   
-    <p>Message is: {{ selected }}</p>
-    </div>
+      <div class="md-layout md-gutter">
+        <div class="md-layout-item md-size-33">
+          <md-field>
+            <label>Modulkürzel</label>
+            <md-input
+              v-model="modKuerzel"
+              name="modKuerzel"
+              id="modKuerzel"
+              md-dense
+              disabled
+            >
+            </md-input>
+          </md-field>
+        </div>
+      </div>
+
+      <div class="md-layout md-gutter">
+        <div class="md-layout-item md-size-50">
+          <md-field>
+            <label>Anzahl Lernergebnisse</label>
+            <md-input v-model="type"></md-input>
+          </md-field>
         </div>
 
-</div>
-</div>
+        <div
+          class="md-layout-item md-size-50"
+          v-for="(input, i) in inputs1"
+          :key="i"
+        >
+          <md-field>
+            <label>Art Lernergebnis</label>
+            <md-input v-model="input.name"></md-input>
+            <span>
+              <i
+                class="fas fa-minus-circle"
+                @click="remove('1', i)"
+                v-show="i || (!i && inputs1.length > 1)"
+              ></i>
+              <i
+                class="fas fa-plus-circle"
+                @click="add('1', i)"
+                v-show="i == inputs1.length - 1"
+              ></i>
+            </span>
+          </md-field>
+        </div>
+      </div>
+
+      <div class="md-layout md-gutter">
+        <div class="md-layout-item md-size-50">
+          <md-field>
+            <label>Anzahl Inhaltselemente</label>
+            <md-input v-model="type"></md-input>
+          </md-field>
+        </div>
+
+        <div
+          class="md-layout-item md-size-50"
+          v-for="(input, i) in inputs2"
+          :key="i"
+        >
+          <md-field>
+            <label>Art Inhaltselement</label>
+            <md-input v-model="input.name"></md-input>
+            <span>
+              <i
+                class="fas fa-minus-circle"
+                @click="remove('2', i)"
+                v-show="i || (!i && inputs2.length > 1)"
+              ></i>
+              <i
+                class="fas fa-plus-circle"
+                @click="add('2', i)"
+                v-show="i == inputs2.length - 1"
+              ></i>
+            </span>
+          </md-field>
+        </div>
+      </div>
+
+      <div class="md-layout md-gutter">
+        <div class="md-layout-item md-size-50">
+          <md-field>
+            <label>Anzahl Prüfungsvorleistungen</label>
+            <md-input v-model="type"></md-input>
+          </md-field>
+        </div>
+
+        <div
+          class="md-layout-item md-size-50"
+          v-for="(input, i) in inputs3"
+          :key="i"
+        >
+          <md-field>
+            <label>Art Prüfungsvorleistungen</label>
+            <md-input v-model="input.name"></md-input>
+            <span>
+              <i
+                class="fas fa-minus-circle"
+                @click="remove('3', i)"
+                v-show="i || (!i && inputs3.length > 1)"
+              ></i>
+              <i
+                class="fas fa-plus-circle"
+                @click="add('3', i)"
+                v-show="i == inputs3.length - 1"
+              ></i>
+            </span>
+          </md-field>
+        </div>
+      </div>
+
+      <div class="md-layout md-gutter">
+        <div class="md-layout-item md-size-50">
+          <md-field>
+            <label>Anzahl Prüfungsleistungen</label>
+            <md-input v-model="type"></md-input>
+          </md-field>
+        </div>
+
+        <div
+          class="md-layout-item md-size-50"
+          v-for="(input, i) in inputs4"
+          :key="i"
+        >
+          <md-field>
+            <label>Art Prüfungsvorleistungen</label>
+            <md-input v-model="input.name"></md-input>
+            <span>
+              <i
+                class="fas fa-minus-circle"
+                @click="remove('4', i)"
+                v-show="i || (!i && inputs4.length > 1)"
+              ></i>
+              <i
+                class="fas fa-plus-circle"
+                @click="add('4', i)"
+                v-show="i == inputs4.length - 1"
+              ></i>
+            </span>
+          </md-field>
+        </div>
+      </div>
+
+      <div class="md-layout md-gutter">
+        <div class="md-layout-item md-size-100">
+          <md-field>
+            <label>Bloomsche Taxonomie</label>
+            <md-select
+              v-model="modType"
+              name="modulType"
+              id="modulType"
+              md-dense
+            >
+              <md-option>erschaffen</md-option>
+              <md-option>evaluieren</md-option>
+              <md-option>analysieren</md-option>
+              <md-option>anwenden</md-option>
+              <md-option>verstehen</md-option>
+              <md-option>erinnern</md-option>
+            </md-select>
+          </md-field>
+        </div>
+      </div>
+    </div>
+    <div class="md-layout md-gutter">
+      <div class="md-layout-item">
+        <div>
+          <md-button type="primary">Speichern</md-button>
+          <md-button type="secondary">Download</md-button>
+          <p>Message is: {{ selected }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-
-    import FormDynamic from "./FormDynamic";
-
+import FormDynamic from "./FormDynamic";
 
 export default {
-    lol: {
-    FormDynamic,
+  lol: {
+    FormDynamic
   },
-    data() {
-        return{
-            selected:"",
-            inputs1: [
-                {
-                    name: ''
-                }
-            ],
-            inputs2: [
-                {
-                    name: ''
-                }
-            ]
+  data() {
+    return {
+      selected: "",
+      inputs1: [
+        {
+          name: ""
         }
+      ],
+      inputs2: [
+        {
+          name: ""
+        }
+      ],
+      inputs3: [
+        {
+          name: ""
+        }
+      ],
+      inputs4: [
+        {
+          name: ""
+        }
+      ],
+    };
+  },
+  methods: {
+    add(input, index) {
+      this["inputs" + input].push({ name: "" });
     },
-    methods: {
-        add(index) {
-            this.inputs1.push({ name: '' });
-        },
-        remove(index) {
-            this.inputs1.splice(index, 1);
-        
+    remove(input, index) {
+      this["inputs" + input].splice(index, 1);
     }
-
-}
-}
-
+  }
+};
 </script>
