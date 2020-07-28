cf4df9256fbe4c07e3449e21117fb1b4997e69e8
Vera Meister <vmeister@univera.de>
2020-07-28 18:09:58 +0200

    split_mhb_py code correction
    Substitution unnötiger Variablen

--- dataPrep/InitialMapping/split_mhb.py
+++ dataPrep/InitialMapping/split_mhb.py
@@ -1,15 +1,11 @@
-mhb_file = open("mhb_gesamt.txt","r",1000,"utf8")
+mhb_file = open("mhb_gesamt.txt","r",encoding="utf8")
 mhb_gesamt = mhb_file.read()
 mhb_file.close()
 modDescrSeq = mhb_gesamt.split("Modulbezeichnung:  ")
-countModDescr = len(modDescrSeq)
-#s = modDescrSeq[1].strip()
-#print(s)
 i = 1
-while i<countModDescr:
+while i<len(modDescrSeq):
     fileName = "ModDescr_" + str(i)
     file = open(fileName + ".txt","w")
-    s = modDescrSeq[i].strip()
-    file.write(s)
+    file.write(modDescrSeq[i].strip())
     file.close()
     i += 1
