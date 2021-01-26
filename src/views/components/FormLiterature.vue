729e76219046e0ab1981775d0547e5da75c2ea88
Philipp Bolte <philipp.bukkit@gmail.com>
2021-01-26 15:58:04 +0000

    rough tab implementation

--- src/views/components/FormLiterature.vue
+++ src/views/components/FormLiterature.vue
@@ -1,3 +1,74 @@
 <template>
-    <label>Hello World</label>
+  <div>
+    <!-- Tabs -->
+    <label>Hier ist eine Menge an Literatur</label>
+
+    <!-- Tabs -->
+    <div>
+      <button
+        v-for="tab in tabs"
+        :key="tab"
+        :class="['tab-button', { active: currentTab === tab }]"
+        @click="currentTab = tab"
+      >
+        {{ tab }}
+      </button>
+      <component :is="currentTab" class="tab" />
+    </div>
+  </div>
 </template>
+
+<script>
+import FormLiteratureDOI from "./FormLiteratureDOI";
+import FormLiteratureISBN from "./FormLiteratureISBN";
+import FormLiteratureManual from "./FormLiteratureManual";
+
+export default {
+  components: {
+    FormLiteratureDOI,
+    FormLiteratureISBN,
+    FormLiteratureManual,
+  },
+  props: [
+    "modLiteratureOrigin",
+    "moduleUri",
+    "role",
+    "newBoolean",
+    "code",
+    "basicFilled ",
+  ],
+  name: "literature",
+  mixins: [],
+  directives: {},
+  data() {
+    return {
+      modLiterature: [],
+      currentTab: "FormLiteratureDOI",
+      tabs: ["FormLiteratureDOI", "FormLiteratureISBN", "FormLiteratureManual"],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.tab-button {
+  padding: 6px 10px;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+  border: 1px solid #ccc;
+  cursor: pointer;
+  background: #f0f0f0;
+  margin-bottom: -1px;
+  margin-right: -1px;
+}
+.tab-button:hover {
+  background: #e0e0e0;
+}
+.tab-button.active {
+  background: #e0e0e0;
+}
+.tab {
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+</style>
---
+++ src/views/components/FormLiteratureManual.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <label> Hey there! Enter all information!</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "literatureManual",
+  data() {
+    return {
+      isActive: true,
+    };
+  },
+};
+</script>
