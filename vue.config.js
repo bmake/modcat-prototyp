553c5c724c56ad848178034e17faf92646de11ad
Philipp Bolte <philipp.bukkit@gmail.com>
2021-02-03 11:42:42 +0000

    enabled cors for dev server

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/modcat/' : '/'
+  publicPath: process.env.NODE_ENV === "production" ? "/modcat/" : "/",
+  devServer: {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
+  },
 };
