7674f3e598436b5e22afbf97b3e401dd1abdf8e6
Wenxin Hu <happyhwx888@googlemail.com>
2021-11-23 13:55:55 +0100

    add main header

---
+++ src/layout/MainHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header-filter" style="background-color: #FF8F00;">
+    <div class="md-layout">
+      <div class="md-layout-item">
+        <div class="index-page">
+          <div class="brand">
+            <img
+              src="../../img/logo.svg"
+              class="img"
+              width="10%"
+              height="auto"
+            />
+            <h1>Modulkatalog @THB</h1>
+            <h3>Fachbereich Wirtschaft</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainHeader"
+};
+</script>
+
+<style scoped>
+.header-filter {
+  padding: 5%;
+}
+
+.brand h3 {
+  margin-top: 0 !important;
+}
+</style>
