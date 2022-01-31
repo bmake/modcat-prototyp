22de9f6eedf99d6eb552cf6cc826acb92382df20
Lena Höding <lena.hoeding@th-brandenburg.de>
2022-01-31 10:56:48 +0100

    changed bloom-goals queries and outcome design

--- src/views/Browsing/ModuleOutcome.vue
+++ src/views/Browsing/ModuleOutcome.vue
@@ -1,68 +1,73 @@
 <template>
   <div class="wrapper">
     <md-content style="border-color: #92d050">
+      
       <div>
+        <h3 style="color: #92d050;"><b>Didaktik</b></h3>
         
-        <div class="container">
-          <!-- Left content -->
-          <div class="container__half" style="border-right: solid 2px #92d050;">
-            <h3 style="color: #92d050;"><b>Didaktik</b></h3>
-            <b>Lernziele nach Kompetenzarten und -stufen: </b>
-            <!-- verursacht unkontrollierte Breite
-            <md-list>
-              <md-list-item v-for="gb in (resultOutcome[0].learnBlooms.value.split('|'))" :key="gb">
-                {{ gb }}
-              </md-list-item>
-            </md-list> -->
+        <div class="goals">
+          <h4>Lernziele</h4>
+          <h5 v-if="(resultCompetenceSubjectmatter !== null)">Fachkompetenz</h5>
             <ul id="goalsAndBlooms">
               <li
-                v-for="gb in resultOutcome[0].learnBlooms.value.split('|')"
-                :key="gb"
+                v-for="(blooms, index) in resultCompetenceSubjectmatter"
+                :key="index"
               >
-                {{ gb.split('@')[0] }}
-              <ul>
-                <li
-                  v-for="bloom in (gb.substring(gb.indexOf('@') + 1)).split('@')"
-                  :key="bloom"
-                >
-                  {{ bloom }}
-                </li>
-              </ul>
+                <b>{{ blooms.bname.value }}</b> - {{ blooms.learnResult.value }}
               </li>
             </ul>
-          </div>
 
-          <!-- Right content -->
-          <div class="container__half">
-            <b>Sozialkompetenz</b>
-            <md-divider
-              style="height:2px; border-width:0; color: #92d050; background-color: #92d050"
-            />
-            <b>Personalkompetenz</b>
-          </div>
+          <h5>Persönliche Kompetenz</h5>
+            <ul id="goalsSelf">
+              <li
+                v-for="(blooms, index) in resultCompetenceSelf"
+                :key="index"
+              >
+                {{ blooms.learnResult.value }}
+              </li>
+            </ul>
+
+          <h5>Sozialkompetenz</h5>
+            <ul id="goalsSocial">
+              <li
+                v-for="(blooms, index) in resultCompetenceSocial"
+                :key="index"
+              >
+                {{ blooms.learnResult.value }}
+              </li>
+            </ul>
+          
         </div>
 
         <md-divider
           style="height:2px; border-width:0; color: #92d050; background-color: #92d050"
         />
-        <b>Strukturierte Lerninhalte: </b>
-        <ul id="contents">
-          <li
-            v-for="co in resultOutcome[0].contents.value.split('|')"
-            :key="co"
-          >
-            {{ co }}
-          </li>
-        </ul>
+
+        <div class="contents">
+          <h4>Lerninhalte</h4>
+          <ul id="contents">
+            <li
+              v-for="co in resultOutcome[0].contents.value.split('|')"
+              :key="co"
+            >
+              {{ co }}
+            </li>
+          </ul>
+        </div>
+
         <md-divider
           style="height:2px; border-width:0; color: #92d050; background-color: #92d050"
         />
-        <b>Prüfungsleistungen: </b>
-        <ul id="exams">
-          <li v-for="ex in resultOutcome[0].exams.value.split('|')" :key="ex">
-            {{ ex }}
-          </li>
-        </ul>
+        
+        <div class="exams">
+          <h4>Prüfungsleistungen</h4>
+          <ul id="exams">
+            <li v-for="ex in resultOutcome[0].exams.value.split('|')" :key="ex">
+              {{ ex }}
+            </li>
+          </ul>
+        </div>
+        
       </div>
     </md-content>
   </div>
@@ -77,15 +82,25 @@ export default {
       resultOutcome: null,
       loading: true,
       errored: false,
-      code: this.$route.params.code
+      code: this.$route.params.code,
+      resultCompetenceSubjectmatter: null,
+      resultCompetenceSelf: null,
+      resultCompetenceSocial: null
     };
   },
   mounted() {
     this.queryOutcome(this.code);
+    this.queryGoalsSubjectmatter(this.code);
+    this.queryGoalsSelf(this.code);
+    this.queryGoalsSocial(this.code);
+    
+  },
+  beforeUpdate() {
+    this.cleanSubjectmatterArray();
   },
   methods: {
     querySparql(query) {
-      //fragt per Axios query, speichert in resultMethod, loading und errored zur Kontrolle
+      //fragt per Axios query, speichert in resultOutcome, loading und errored zur Kontrolle
       axios
         .post(
           "http://fbw-sgmwi.th-brandenburg.de:3030/RelaunchJuly20_ModCat/query",
@@ -162,8 +177,136 @@ export default {
         "    } ORDER BY ?contentPos" +
         "} " +
         "}";
-      console.log(query);
+      //console.log(query);
       this.querySparql(query);
+    },
+    //3 very similar methods, can be optimised
+    queryGoalsSubjectmatter(code) {
+      let query =
+        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+        "PREFIX module: <https://bmake.th-brandenburg.de/module/> " +
+        "PREFIX schema: <https://schema.org/> " +
+        "SELECT DISTINCT ?code ?bname ?addList ?learnResult " +
+        "WHERE { " +
+        '  module:' +
+        code +
+         ' schema:courseCode ?code ; ' +
+        "    schema:name ?label ." +
+        '    FILTER(lang(?label) = "de")' +
+          
+        '  module:LResults_' +
+        code +
+        ' schema:itemListElement ?resList .' +
+        "                  ?resList schema:description ?learnResult ;" +
+        "                      schema:position ?position ." +
+        "                  ?resList schema:additionalType ?addList ;" +
+        "                          schema:additionalType module:SubjectMatterCompetence." +
+        "            ?addList schema:name ?bname." +
+        '    FILTER(lang(?bname) = "de") ' +     
+        "}";
+
+      axios
+        .post(
+          "http://fbw-sgmwi.th-brandenburg.de:3030/RelaunchJuly20_ModCat/query",
+          query,
+          {
+            headers: { "Content-Type": "application/sparql-query" }
+          }
+        )
+        .then(response => {
+          this.resultCompetenceSubjectmatter = response.data.results.bindings;
+        })
+        .catch(e => {
+          this.errors.push(e);
+        });
+    },
+    queryGoalsSelf(code) {
+      let query =
+        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+        "PREFIX module: <https://bmake.th-brandenburg.de/module/> " +
+        "PREFIX schema: <https://schema.org/> " +
+        "SELECT DISTINCT ?code ?bname ?addList ?learnResult " +
+        "WHERE { " +
+        '  module:' +
+        code +
+         ' schema:courseCode ?code ; ' +
+        "    schema:name ?label ." +
+        '    FILTER(lang(?label) = "de")' +
+          
+        '  module:LResults_' +
+        code +
+        ' schema:itemListElement ?resList .' +
+        "                  ?resList schema:description ?learnResult ;" +
+        "                      schema:position ?position ." +
+        "                  ?resList schema:additionalType ?addList ;" +
+        "                          schema:additionalType module:SelfCompetence." +
+        "            ?addList schema:name ?bname." +
+        '    FILTER(lang(?bname) = "de") ' +     
+        "}";
+      
+      axios
+        .post(
+          "http://fbw-sgmwi.th-brandenburg.de:3030/RelaunchJuly20_ModCat/query",
+          query,
+          {
+            headers: { "Content-Type": "application/sparql-query" }
+          }
+        )
+        .then(response => {
+          this.resultCompetenceSelf= response.data.results.bindings;
+        })
+        .catch(e => {
+          this.errors.push(e);
+        });
+    },
+    queryGoalsSocial(code) {
+      let query =
+        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+        "PREFIX module: <https://bmake.th-brandenburg.de/module/> " +
+        "PREFIX schema: <https://schema.org/> " +
+        "SELECT DISTINCT ?code ?bname ?addList ?learnResult " +
+        "WHERE { " +
+        '  module:' +
+        code +
+         ' schema:courseCode ?code ; ' +
+        "    schema:name ?label ." +
+        '    FILTER(lang(?label) = "de")' +
+          
+        '  module:LResults_' +
+        code +
+        ' schema:itemListElement ?resList .' +
+        "                  ?resList schema:description ?learnResult ;" +
+        "                      schema:position ?position ." +
+        "                  ?resList schema:additionalType ?addList ;" +
+        "                          schema:additionalType module:SocialCompetence." +
+        "            ?addList schema:name ?bname." +
+        '    FILTER(lang(?bname) = "de") ' +     
+        "}";
+
+      axios
+        .post(
+          "http://fbw-sgmwi.th-brandenburg.de:3030/RelaunchJuly20_ModCat/query",
+          query,
+          {
+            headers: { "Content-Type": "application/sparql-query" }
+          }
+        )
+        .then(response => {
+          this.resultCompetenceSocial = response.data.results.bindings;
+        })
+        .catch(e => {
+          this.errors.push(e);
+        });
+    },
+    cleanSubjectmatterArray() {
+      let cleanArray = [];
+      for (let index = 0; index < this.resultCompetenceSubjectmatter.length; index++) {
+        const element = this.resultCompetenceSubjectmatter[index];
+        if (element.bname.value != "Fachkompetenz") {
+          cleanArray.push(element);
+        }
+      }
+      this.resultCompetenceSubjectmatter = cleanArray;
     }
   }
 };
@@ -179,13 +322,5 @@ export default {
   border-style: solid;
   margin: 2px;
   padding: 5px;
-}
-
-.container {
-  display: flex;
-}
-
-.container__half {
-  flex: 1;
 }
 </style>
